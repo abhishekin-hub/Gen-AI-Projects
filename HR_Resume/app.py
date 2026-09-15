e39a87dd785c31191f14f9e4518f557527704e2e
abhishekin-hub <abhishek.inspn@gmail.com>
2026-09-15 21:08:43 +0530

    Update resume upload process in app.py

--- HR_Resume/app.py
+++ HR_Resume/app.py
@@ -1,20 +1,23 @@
 import streamlit as st
 from utils import *
 
+
 st.set_page_config(page_title="HR Assistant", page_icon='robot')
-st.header("HR-Resume Screening Assistant")
-st.subheader("I find perfect match for your requirements")
+st.header("Resume Screening Assistant")
+st.write("**Let me find the perfect match for your requirements.**")
 
 requirements = st.text_area("Share the job requirements")
 no_of_job_openings = st.slider("How many matches are you looking for?", 1,3,1)
 
 with st.sidebar:
-    resumes = st.file_uploader("Upload resumes (PDF Only)", type=["pdf"], accept_multiple_files=True)
+    st.subheader("Upload Resumes:")
+    resume_folder = st.text_input("Path to resumes folder", value="C:/Users/AB263004/Downloads/RAG_HR_Resumes_Dump")
+    uploaded_files = st.file_uploader("Upload resumes (PDF Only)", type=["pdf"], accept_multiple_files=True)
 
 Submit = st.button("Assist me")
 
 if Submit:
     with st.spinner("Wait for it..."):
 
-        response = process(resumes, requirements, no_of_job_openings)
+        response = process(resume_folder, uploaded_files, requirements, no_of_job_openings)
         st.write(response)
